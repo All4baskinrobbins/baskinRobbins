97f530b014af879c6554de54bdc4aadb59560cdc
ppspai <kidone0033@gmail.com>
2023-02-07 21:48:40 +0900

    D-1 v1.1

--- src/main/java/baskin/back/domain/Users.java
+++ src/main/java/baskin/back/domain/Users.java
@@ -29,10 +29,8 @@ public class Users {
     @NotBlank
     private String name;
     @NotBlank
-    @Pattern(regexp = "[a-zA-Z0-9]]{4,12}", message = "아이디는 영어 대소문자, 숫자를 이용하여 4자 이상 12자 이하로 적어주세요")
     private String userid;
     @NotBlank
-    @Pattern(regexp = "[a-zA-Z0-9]{8,12}", message = "비밀번호는 영어 대소문자, 숫자를 이용하여 8자 이상 12자 이하로 적어주세요")
     private String userpw;
 
     private Long birth;
